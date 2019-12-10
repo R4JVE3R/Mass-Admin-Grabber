7fcf5bb830aa9947660d5791f5c1817df3ad99e8
Rajveer <rajveervihol@gmail.com>
2019-12-10 22:07:52 +0530

    Update MassAdminGrabber.py

--- MassAdminGrabber.py
+++ MassAdminGrabber.py
@@ -1,56 +1,84 @@
 import urllib3
 import time
 import os
-from colorama import Fore
+from colorama import Fore,init
+import threading
 
-possibleAdmin = ['admin','Admin','administrator','adminpanel','webadmin','wp-admin','admin.php','login.php','Login.php']
-foundAdminList = []
+init(autoreset=True)
 print(Fore.RED+'''
 █▀▄▀█ █▀▀█ █▀▀ █▀▀   █▀▀█ █▀▀▄ █▀▄▀█ ░▀░ █▀▀▄   █▀▀▀ █▀▀█ █▀▀█ █▀▀▄ █▀▀▄ █▀▀ █▀▀█
 █░▀░█ █▄▄█ ▀▀█ ▀▀█   █▄▄█ █░░█ █░▀░█ ▀█▀ █░░█   █░▀█ █▄▄▀ █▄▄█ █▀▀▄ █▀▀▄ █▀▀ █▄▄▀
 ▀░░░▀ ▀░░▀ ▀▀▀ ▀▀▀   ▀░░▀ ▀▀▀░ ▀░░░▀ ▀▀▀ ▀░░▀   ▀▀▀▀ ▀░▀▀ ▀░░▀ ▀▀▀░ ▀▀▀░ ▀▀▀ ▀░▀▀
 
                                                             - By R̴4̴J̴V̴E̴3̴R̴  ♥
-'''+Fore.RESET)
+''')
 print('''
-================================================
+==================================================
 [~]Facebook  : https://facebook.com/R4JVE3R/
 [~]Instagram : https://instagram.com/R4JVE3R/
 [~]Twitter   : https://twitter.com/R4JVE3R/
-================================================
+==================================================
 ''')
 
-http = urllib3.PoolManager()
-siteList = open("list.txt","r")
-result = open('Result.txt','w')
-error = open('error.txt','w')
 if(os.stat("list.txt").st_size == 0 or os.stat("list.txt").st_size == 1):
-    print(Fore.RED+"List.txt File is Empty Please Enter Your Targets Inside It.")
+    print(Fore.RED+"List.txt File is Empty Please Enter Your Targets Inside It.\n")
     exit()
-total = siteList.readlines()
 
-for site in total:
-    print("[-] "+site.rstrip('\n')+" Scanning...")
+http = urllib3.PoolManager()
+possibleAdmin = [
+    'admin','Admin','login','Login','administrator','Administrator','adminpanel','webadmin','wp-admin','admin.php','login.php','Login.php','admincontrol','pages/admin','siteadmin',
+    'webmaster','UserLogin','myadmin','members','user','adminLogin.php']
+foundAdminList = []
+foundAdminList1 = []
+
+def Boost(LengthForThis):
+    for site in range(LengthForThis):
+        print("[*] "+total[site].rstrip('\n')+"  :  "+Fore.BLUE+"Scanning...")
+
+        for admin in possibleAdmin:
+            req = total[site].rstrip('\n')+"/"+admin
+            try:
+                respo = http.request('GET',req)  
+            except urllib3.exceptions.MaxRetryError as identifier:
+                error.write("Exception Occured at "+req+'\n')
+            if(respo.status == 200 or respo.status == 302):
+                foundAdminList1.append(req)
+                break
+    
+
+siteList = open("list.txt","r")
+result = open('result.txt','w')
+error = open('error.txt','w')
+
+total = siteList.readlines()
+HalfLength = (len(total))/2
+t1 = threading.Thread(target=Boost,args=(int(HalfLength),))
+t1.start()
+time.sleep(0.3)
+
+for site in range(int(HalfLength),int(HalfLength+HalfLength)):
+    print("[*] "+total[site].rstrip('\n')+"  :  "+Fore.BLUE+"Scanning...")
+
     for admin in possibleAdmin:
-        req = site.rstrip('\n')+"/"+admin
+        req = total[site].rstrip('\n')+"/"+admin
         try:
             respo = http.request('GET',req)  
         except urllib3.exceptions.MaxRetryError as identifier:
             error.write("Exception Occured at "+req+'\n')
-        except:
-            pass
         if(respo.status == 200 or respo.status == 302):
             foundAdminList.append(req)
             break
 
-print("\n\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+print("\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++")
 print("+ Here is List of Sites Where i Found Admin Panel +")
-print("+++++++++++++++++++++++++++++++++++++++++++++++++++\n\n\n")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++\n\n")
 time.sleep(2)
 
 for site in foundAdminList:
     print(Fore.GREEN+"[+] "+site)
     result.write(site+'\n')
-
-print('\n\n++++++++++Done Bro++++++++++')
-print('All Sites with Admin Panel Saved in Result.txt\n')
+for site in foundAdminList1:
+    print(Fore.GREEN+"[+] "+site)
+    result.write(site+'\n')
+print('\n\n++++++++++Done Bro++++++++++\n')
